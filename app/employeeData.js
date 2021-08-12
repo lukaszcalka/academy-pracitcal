4ce07b497cd68f8a51fb794b176b62e4decc58b6
ymtyroni <lukasz.calka@protonmail.com>
2021-08-12 13:24:20 +0200

    add API for Project table

--- app/employeeData.js
+++ app/employeeData.js
@@ -39,3 +39,17 @@ exports.addSalesEmployee = async (Sales) => {
 exports.getReport = async () => {
     return await db.query("select emp_first_name, emp_last_name, emp_bu from employee order by emp_bu;")
 }
+
+const getProjects = async () => {
+    return await db.query(
+        "SELECT * FROM project",[])
+    
+}
+
+exports.getProjects = getProjects;
+
+exports.addProject = async (Project) =>
+{
+    const result = await db.query("INSERT INTO project SET ?", Project);
+     return result.insertId;
+}

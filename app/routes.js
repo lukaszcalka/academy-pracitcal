2c9a46a1b109ce71cc9f73b3d1d20496126eecc6
Maciej Trząsalski <maciej.trzasalski@kainos.com>
2021-08-12 11:33:55 +0200

    hr-menu added

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const employeeData = require('./employeeData.js');
 
 router.get('/list-employees', async (req, res) => {
-    const emps = await employeeData.getEmployees()
-    console.log(emps)
     res.render('list-employees', {employees: await employeeData.getEmployees() } );
   })
+
+router.get('/hr-menu', async (req, res) => {
+    res.render('hr-menu');
+})
 
 // router.post('/new-employee', async (req, res) => {
 //     res
